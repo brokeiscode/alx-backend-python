7a41427701b82e78e5f28a849544d560618b6e8e
Olakunle Simeon Owolabi <owolabi.olakunlesimeon@gmail.com>
2025-07-02 19:50:10 +0100

    batch_processing commit to python-generators-0x00

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,26 @@
+from mysql.connector import Error
+import seed
+
+
+def stream_users_in_batches(batch_size):
+    try:
+        connection = seed.connect_to_prodev()
+        if connection:
+            cursor = connection.cursor(dictionary=True)
+            cursor.execute('select * from user_data')
+            while True:
+                rows = cursor.fetchmany(batch_size)
+                if not rows:
+                    break  # rows finished
+                yield rows
+            cursor.close()
+            connection.close()
+    except Error as e:
+        print(f"Error streaming data from MySQL: {e}")
+
+
+def batch_processing(batch_size):
+    for rows in stream_users_in_batches(batch_size):
+        for user in rows:
+            if user.get('age') > 25:
+                print(user)
