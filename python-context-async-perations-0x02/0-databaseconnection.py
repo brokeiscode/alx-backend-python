a3b2a2ae5b7e320bfe159de14d2726114f105d03
Olakunle Simeon Owolabi <owolabi.olakunlesimeon@gmail.com>
2025-07-09 11:40:04 +0100

    0-databaseconnection commit to python-context-async-perations-0x02

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+
+class DatabaseConnection:
+    def __init__(self, db_name: str):
+        self.db_name = db_name
+        self.conn = None
+
+    def __enter__(self):
+        print(f"connecting to sqlite3 database: {self.db_name}...")
+        try:
+            self.conn = sqlite3.connect(self.db_name)
+            return self.conn.cursor()
+        except sqlite3.Error as e:
+            print(f"Error connecting to the db: {e}")
+            raise
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type and exc_tb:
+            print("Error Occurred:\n", exc_val)
+        else:
+            pass
+        return True
+
+
+with DatabaseConnection('users.db') as cursor:
+    cursor.execute("SELECT * FROM users")
+    result = cursor.fetchall()
+    print(result)
