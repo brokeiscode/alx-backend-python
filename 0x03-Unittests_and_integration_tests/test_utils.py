71b426ab59c4652e6bb8a2e16928f0a91c75b3a0
Olakunle Simeon Owolabi <owolabi.olakunlesimeon@gmail.com>
2025-07-15 13:25:03 +0100

    TestAccessNestedMap commit to test_utils

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Testing the classes in the Utils modules
+"""
+
+import unittest
+
+from parameterized import parameterized
+
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """TestCase for the Function access_nested_map
+    """
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """
+        Test nested_map with the keys in path provided
+        :param nested_map: a nested dictionary.
+        :param path: tuple of the nested keys.
+        :param expected: the expected result values.
+        :return:
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
