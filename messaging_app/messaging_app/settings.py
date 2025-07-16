0bd334b323f772e7fa14df4e38017fcc59fac980
Olakunle Simeon Owolabi <owolabi.olakunlesimeon@gmail.com>
2025-07-16 16:23:35 +0100

    create new app

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z%evu_qn%qj6dsi&df31@*s)fs@w7$5+4b=%(p93#^exj3l*%9'
+SECRET_KEY = 'django-insecure-z%evu_qn%qj6dsi&df31@*s)fs@w7$5+4b=xj3l*%9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
